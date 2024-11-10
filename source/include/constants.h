f80c82afc5de5c676699010997389f455217fcb6
Jibril Sharafi <jibril.sharafi@gmail.com>
2024-11-10 19:52:12 +0100

    Bigger MQTT payloads

--- source/include/constants.h
+++ source/include/constants.h
@@ -3,7 +3,7 @@
 // Firmware info
 #define FIRMWARE_BUILD_VERSION_MAJOR "00"
 #define FIRMWARE_BUILD_VERSION_MINOR "07"
-#define FIRMWARE_BUILD_VERSION_PATCH "03"
+#define FIRMWARE_BUILD_VERSION_PATCH "04"
 #define FIRMWARE_BUILD_VERSION FIRMWARE_BUILD_VERSION_MAJOR "." FIRMWARE_BUILD_VERSION_MINOR "." FIRMWARE_BUILD_VERSION_PATCH
 
 #define FIRMWARE_BUILD_DATE __DATE__
@@ -92,13 +92,13 @@
 #define MAX_INTERVAL_STATUS_PUBLISH 3600000 // The interval between two status publish (in milliseconds)
 #define MAX_INTERVAL_CRASH_MONITOR_PUBLISH 3600000 // The interval between two status publish (in milliseconds)
 #define MQTT_MAX_CONNECTION_ATTEMPT 3 // The maximum number of attempts to connect to the MQTT server
-#define MQTT_OVERRIDE_KEEPALIVE 30 // Minimum value supported by AWS IoT Core (in seconds) 
+#define MQTT_OVERRIDE_KEEPALIVE 60 // Minimum value supported by AWS IoT Core (in seconds) 
 #define MQTT_MIN_CONNECTION_INTERVAL 5000 // Minimum interval between two connection attempts (in milliseconds)
 #define MQTT_TEMPORARY_DISABLE_INTERVAL 60000 // Interval between reconnect attempts after a failed connection (in milliseconds)
 #define MQTT_TEMPORARY_DISABLE_ATTEMPTS 3 // The maximum number of temporary disable attempts before erasing the certificates and restarting the ESP32
 #define MQTT_LOOP_INTERVAL 100 // Interval between two MQTT loop checks (in milliseconds)
-#define PAYLOAD_METER_MAX_NUMBER_POINTS 30 // The maximum number of points that can be sent in a single payload
-#define MQTT_PAYLOAD_LIMIT 8192 // Increase the base limit of 256 bytes
+#define PAYLOAD_METER_MAX_NUMBER_POINTS 150 // The maximum number of points that can be sent in a single payload. Going higher than about 150 leads to unstable connections
+#define MQTT_PAYLOAD_LIMIT 32768 // Increase the base limit of 256 bytes. Increasing this over 32768 bytes will lead to unstable connections
 #define MQTT_MAX_TOPIC_LENGTH 256 // The maximum length of a MQTT topic
 #define MQTT_PROVISIONING_TIMEOUT 60000 // The timeout for the provisioning response (in milliseconds)
 #define MQTT_PROVISIONING_LOOP_CHECK 1000 // Interval between two certificates check on memory (in milliseconds)
